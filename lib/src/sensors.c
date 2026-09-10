cb91f31f512e1c5cdf8c2dd3c8c914b6895c3d24
Chris-0307 <135561620+Chris-0307@users.noreply.github.com>
2026-09-10 13:04:33 -0600

    CMake + implementaciones basicas

---
+++ lib/src/sensors.c
@@ -0,0 +1,19 @@
+#include "robot_hw.h"
+
+
+float robot_get_front_distance(void)
+{
+    return 0.0f;
+}
+
+
+float robot_get_left_distance(void)
+{
+    return 0.0f;
+}
+
+
+float robot_get_right_distance(void)
+{
+    return 0.0f;
+}
