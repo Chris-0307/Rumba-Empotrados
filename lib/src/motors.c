6b39977247a28c97639c7e45b61b2ac2f8a4ffc4
Chris-0307 <135561620+Chris-0307@users.noreply.github.com>
2026-09-10 14:07:21 -0600

    Logica de control de motores en lib

--- lib/src/motors.c
+++ lib/src/motors.c
@@ -1,13 +1,52 @@
 #include "robot_hw.h"
+#include "motor_backend.h"
+
+
+static int valid_speed(int speed)
+{
+    return speed >= -100 && speed <= 100;
+}
+
+
+static int valid_movement_speed(int speed)
+{
+    return speed >= 0 && speed <= 100;
+}
 
 
 int robot_set_motor_speeds(int left_speed, int right_speed)
 {
-    if (left_speed < -100 || left_speed > 100 ||
-        right_speed < -100 || right_speed > 100)
+    int result;
+
+
+    if (!valid_speed(left_speed) ||
+        !valid_speed(right_speed))
     {
         return ROBOT_INVALID_ARGUMENT;
     }
+
+
+    result = motor_backend_set_speed(
+        MOTOR_LEFT,
+        left_speed
+    );
+
+    if (result != ROBOT_OK)
+    {
+        return result;
+    }
+
+
+    result = motor_backend_set_speed(
+        MOTOR_RIGHT,
+        right_speed
+    );
+
+    if (result != ROBOT_OK)
+    {
+        return result;
+    }
+
 
     return ROBOT_OK;
 }
@@ -15,49 +54,64 @@ int robot_set_motor_speeds(int left_speed, int right_speed)
 
 int robot_move_forward(int speed)
 {
-    if (speed < 0 || speed > 100)
+    if (!valid_movement_speed(speed))
     {
         return ROBOT_INVALID_ARGUMENT;
     }
 
-    return ROBOT_OK;
+    return robot_set_motor_speeds(
+        speed,
+        speed
+    );
 }
 
 
 int robot_move_backward(int speed)
 {
-    if (speed < 0 || speed > 100)
+    if (!valid_movement_speed(speed))
     {
         return ROBOT_INVALID_ARGUMENT;
     }
 
-    return ROBOT_OK;
+    return robot_set_motor_speeds(
+        -speed,
+        -speed
+    );
 }
 
 
 int robot_turn_left(int speed)
 {
-    if (speed < 0 || speed > 100)
+    if (!valid_movement_speed(speed))
     {
         return ROBOT_INVALID_ARGUMENT;
     }
 
-    return ROBOT_OK;
+    return robot_set_motor_speeds(
+        -speed,
+        speed
+    );
 }
 
 
 int robot_turn_right(int speed)
 {
-    if (speed < 0 || speed > 100)
+    if (!valid_movement_speed(speed))
     {
         return ROBOT_INVALID_ARGUMENT;
     }
 
-    return ROBOT_OK;
+    return robot_set_motor_speeds(
+        speed,
+        -speed
+    );
 }
 
 
 int robot_stop(void)
 {
-    return ROBOT_OK;
+    return robot_set_motor_speeds(
+        0,
+        0
+    );
 }
--- lib/src/robot_hw.c
+++ lib/src/robot_hw.c
@@ -1,12 +1,27 @@
 #include "robot_hw.h"
+#include "motor_backend.h"
 
 
 int robot_init(void)
 {
+    int result;
+
+
+    result = motor_backend_init();
+
+    if (result != ROBOT_OK)
+    {
+        return result;
+    }
+
+
     return ROBOT_OK;
 }
 
 
 void robot_cleanup(void)
 {
+    robot_stop();
+
+    motor_backend_cleanup();
 }
